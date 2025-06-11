3ea968401696f0d1c7f87cbfcf657e51f31adca0
Daniele Costa <costadaniele2016@gmail.com>
2025-06-11 11:19:42 +0200

    Aggiunta: Classe BaseEntity; Modifica: Classe Utente

---
+++ src/main/java/entity/BaseEntity.java
@@ -0,0 +1,35 @@
+package entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+@ToString
+public abstract class BaseEntity {
+	
+	@CreatedDate
+	@Column(updatable = false)
+	@CreationTimestamp
+	private LocalDateTime dataCreazione;
+	
+	@LastModifiedDate
+	@Column(insertable = false)
+	@UpdateTimestamp
+	private LocalDateTime dataModifica;
+	
+}
--- src/main/java/entity/Utente.java
+++ src/main/java/entity/Utente.java
@@ -1,8 +1,20 @@
 package entity;
 
-public class Utente {
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor
+@Table(name = "utente")
+public class Utente extends BaseEntity {
 	
-	private long id;
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(unique = true)
+	private int codice;
+	
 	private String nome;
 	private String cognome;
 	private int eta;
