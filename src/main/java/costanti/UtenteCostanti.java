4953fd514c641aae51187c6cf79a23630bc7fc18
Daniele Costa <costadaniele2016@gmail.com>
2025-06-11 17:32:58 +0200

    Modifica: application.yml, UtenteCostanti

--- src/main/java/costanti/UtenteCostanti.java
+++ src/main/java/costanti/UtenteCostanti.java
@@ -7,7 +7,7 @@ public class UtenteCostanti {
     public static final String STATUS_200 = "200";
     public static final String STATUS_200_MESSAGE = "Operazione completata";
     public static final String STATUS_201 = "201";
-    public static final String STATUS_201_MESSAGE = "Astronave creata con successo";
+    public static final String STATUS_201_MESSAGE = "Utente registrato con successo";
     public static final String STATUS_500 = "500";
     public static final String STATUS_500_MESSAGE = "Errore del server";
 	
